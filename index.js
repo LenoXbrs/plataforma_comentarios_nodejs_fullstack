c49930aa2b1bf6e5d65854680e2a52975a8fbf8f
Luis Eduardo <banedycraft@gmail.com>
2023-04-19 23:45:25 -0300

    add rota do form

--- index.js
+++ index.js
@@ -5,38 +5,21 @@ const app = express();
 app.set('view engine','ejs');
 app.use(express.static('public')) //carregar css
 
-app.get("/:nome/:lang",(req,res)=> {
+app.get("/",(req,res)=> {
   
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-
-    var produtos = [
-    {nome: "doritos", preco: 5.14,},
-    {nome: "coca-cola",preco:11.50,},
-    { nome:"Leite",preco: 6.29,}
-    ]
-    
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Savage Code",
-        inscritos: 8000 ,
-        msg : exibirMsg,
-        produtos : produtos
-    })
-})
-
-
-app.get('/login',(req,res)=>{
-
-
-    res.render('login-page')
+    res.render('index')
 
 })
 
 
+app.get('/comentar',(req,res)=>{
+    res.render('comentar')
+})
+
+//usado no formulario
+app.post('/salvarpergunta',(req,res)=>{
+
+})
 
 
 app.listen(8080,()=>{
