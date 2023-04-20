7de5fef6d5a91285c2ab3a58928d51f5fd0ba3d6
Luis Eduardo <banedycraft@gmail.com>
2023-04-19 22:39:27 -0300

    testa js c ejs

--- index.js
+++ index.js
@@ -3,20 +3,39 @@ const express = require('express');
 const app = express();
 
 app.set('view engine','ejs');
+app.use(express.static('public')) //carregar css
 
-app.get("/",(req,res)=> {
-    var inscritos = 8000;
-    var nome = "Luis";
-    var lang = "Java";
+app.get("/:nome/:lang",(req,res)=> {
+  
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+
+    var produtos = [
+    {nome: "doritos", preco: 5.14,},
+    {nome: "coca-cola",preco:11.50,},
+    { nome:"Leite",preco: 6.29,}
+    ]
     
 
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Savage Code",
-        inscritos: inscritos ,
+        inscritos: 8000 ,
+        msg : exibirMsg,
+        produtos : produtos
     })
 })
+
+
+app.get('/login',(req,res)=>{
+
+
+    res.render('login-page')
+
+})
+
 
 
 
