59f866c6372b1e0b0e29b5ccbf55081e86a58f25
Luis Eduardo <banedycraft@gmail.com>
2023-04-21 14:40:49 -0300

    findOne

--- index.js
+++ index.js
@@ -59,6 +59,22 @@ app.post('/salvarpergunta',(req,res)=>{
 })
 
 
+app.get('/pergunta/:id',(req,res)=>{
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta =>{
+        if(pergunta != undefined){
+            res.render('pergunta',{
+               pergunta: pergunta
+            })
+        }else{
+            res.redirect('/');
+        }
+    })
+});
+
+
 app.listen(8080,()=>{
     console.log("App rodando!");
 });
